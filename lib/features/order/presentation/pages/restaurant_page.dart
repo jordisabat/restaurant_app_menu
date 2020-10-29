65c19e6ff8e922a5b700b49b517c09d5cd05ec4e
Jordi Sabat <jsabat@gmail.com>
2020-10-29 11:34:54 +0100

    add dependency injection

---
+++ lib/features/order/presentation/pages/restaurant_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RestaurantPage extends StatelessWidget {
+  const RestaurantPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Prodcuts and orders'),
+    );
+  }
+}
